1808712bc73537ccb6e3d6bd0fe2d903dce163e3
Letícia Schmitz <137583118+lelefernanda08@users.noreply.github.com>
2025-04-16 11:30:28 +0000

    BotaoAcessibilidade

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 document,addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+      botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+      opcoesAcessibilidade.classList.toggle('apresenta-lista');
+    })
+    
     let tamanhoAtualFonte = 1;
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
@@ -13,7 +21,5 @@ document,addEventListener('DOMContentLoaded', function(){
     diminuirFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -=0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    })
-    
-    
+    }) 
 })
