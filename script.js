57017cc220a3312bc3c477f40059a3a42ee0ffcc
Letícia Schmitz <137583118+lelefernanda08@users.noreply.github.com>
2025-04-23 08:43:37 -0300

    cores do contraste

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ document,addEventListener('DOMContentLoaded', function(){
       botaoDeAcessibilidade.classList.toggle('rotacao-botao');
       opcoesAcessibilidade.classList.toggle('apresenta-lista');
     })
+
+    const alternaContraste = document.getElementById('alterna-contraste')
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
     
     let tamanhoAtualFonte = 1;
 
